1b6baddfec53c79738ca686d90a1c6d8bb83c1d3
Tarasios <sircassiel@gmail.com>
2024-09-11 21:18:19 -0700

    Update user.js

--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -2,7 +2,8 @@ const MESSAGES = {
     userPromptMessage: "How many buttons to create?",
     invalidInputMessage: "Please enter a number between 3 and 7.",
     successMessage: "Excellent memory!",
-    failureMessage: "Wrong order! Game over."
+    failureMessage: "Wrong order! Game over.",
+    goButtonText: "Go"
 };
 
 export default MESSAGES;
