57358b3925629353d1aab44f847bb27a47e241ce
Tarasios <cassiel@glennwilliams.ca>
2024-09-15 23:10:13 -0700

    Add Lab1

---
+++ lang/messages/en/lab1Messages.js
@@ -0,0 +1,13 @@
+const MESSAGES = {
+    addButtonText: "Add Note",
+    removeButtonText: "Remove",
+    backButtonText: "Back",
+    lastUpdatedText: "Updated at:",
+    lastSavedText: "Stored at:",
+    pageTitle: "Lab 2: JSON and LocalStorage",
+    writerPageLinkText: "Go to Writer Page",
+    readerPageLinkText: "Go to Reader Page",
+    defaultStudentName: "Student: [Name not set]"
+};
+
+export default MESSAGES;
